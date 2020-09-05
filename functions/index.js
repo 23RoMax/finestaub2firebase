08007c7cdd6bf7ae73b0a035e5e7b4894c379ce9
23RoMax <max.nebl@googlemail.com>
2020-09-05 16:18:13 +0200

    feat: store data in firestore

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,15 @@
 //////////////////////////////////////////////////
 
 const functions = require('firebase-functions')
+const firebase = require('firebase')
+const admin = require('firebase-admin')
+require("firebase/firestore")
 const express = require('express')
 const cors = require('cors')
 
 const app = express()
+admin.initializeApp()
+const db = admin.firestore()
 
 app.use(cors())
 /* eslint-disable*/
@@ -31,9 +36,52 @@ app.get('/data', function (req, res) {
   return res.status(200).send("Hi")
 })
 
-app.post('/data', function (req, res) {
-  console.log(req.body)
-  return res.status(200).send("Hi")
+app.post('/data', async function (req, res) {
+  let responseObject = {
+    msg: null,
+    status: null,
+    count: 0,
+    data: null
+  }
+  
+  async function addMeasurement (sensor, sensorData) {
+    db.collection(sensor).add(sensorData)
+    .then(docRef => {
+      console.log('Storing sensor data of', sensor)
+      console.log(sensorData)
+      console.log("Document written with ID: ", docRef.id)
+      responseObject.status = 200
+      responseObject.count = 1
+      responseObject.msg = 'Created Sensordata successfully'
+      responseObject.data = sensorData
+      return res.status(200).send(responseObject)
+    })
+    .catch(error => {
+      console.error("Error adding document: ", error)
+      responseObject.msg = error
+      responseObject.status = 400
+    })
+  }
+
+  try {
+
+    const sensor = req.body.esp8266id
+
+    let now = new Date().toISOString()
+
+
+    var sensorData = {
+      createdAt: now,
+      updatedAt: now,
+      ...req.body.sensordatavalues
+    }
+
+    await addMeasurement(sensor, sensorData)
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send(error)
+  }
 })
 const api = functions.https.onRequest(app)
 
